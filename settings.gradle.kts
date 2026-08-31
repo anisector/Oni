c6386a78bfb1a6348f49c6a0f9aa419152e205da
anisector <144573755+anisector@users.noreply.github.com>
2026-08-31 04:22:12 +0300

    Add Gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal(); maven("https://jitpack.io") }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories { google(); mavenCentral(); maven("https://jitpack.io") }
+}
+rootProject.name = "oni-anizium"
+include(":Oni")
