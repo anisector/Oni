6a17b7be98c3c706f8ff52da17737e6353a50777
anisector <144573755+anisector@users.noreply.github.com>
2026-08-31 04:22:23 +0300

    Add Anizium module configuration

---
+++ Oni/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 32
+cloudstream {
+    description = "Anizium provider - current official app API routes and resilient headers"
+    authors = listOf("kerimmkirac", "ByAyzen", "anisector")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie")
+    language = "tr"
+    iconUrl = "https://anizium.co/assets/images/logo.png"
+}
